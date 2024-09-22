e70216ddf09f4fc3f22b3f6d7638b59d55a69308
Biswajit <biswajit.swain@zysk.tech>
2024-09-22 11:27:15 +0530

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <div className="text-2xl text-white underline">Hello!</div>
+    </main>
+  );
+};
+
+export default App;
