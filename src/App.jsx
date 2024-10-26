d972aa2364ef31c87b9ea2f5c43aa7e8fb1e41e0
Biswajit <biswajit.swain@zysk.tech>
2024-10-26 07:05:30 +0530

    Done: Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./sections/About";
 import Projects from "./sections/Projects";
 import Clients from "./sections/Clients";
 import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Projects />
       <Clients />
       <Contact />
+      <Footer />
     </main>
   );
 };
